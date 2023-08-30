a99bce51a8882faf2702e792f7e5f444b6d712ed
KayB <keziahbaafi@gmail.com>
2023-08-30 02:37:41 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,12 +15,6 @@ const listint_t **r(const listint_t **list, size_t size, const listint_t *new)
 	const listint_t **new_list;
 	size_t k;
 
-	for (k = 0; k < size - 1; k++)
-		new_list[k] = list[k];
-	new_list[k] = new;
-	free(list);
-	return (new_list);
-
 	new_list = malloc(size * sizeof(listint_t *));
 	if (new_list == NULL)
 	{
@@ -28,6 +22,11 @@ const listint_t **r(const listint_t **list, size_t size, const listint_t *new)
 		exit(98);
 	}
 
+	for (k = 0; k < size - 1; k++)
+		new_list[k] = list[k];
+	new_list[k] = new;
+	free(list);
+	return (new_list);
 }
 /**
  * print_listint_safe - a function that prints a listint_t linked list
